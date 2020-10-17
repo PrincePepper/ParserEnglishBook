4a87440a188252dfb2a3b732df70f5626acd2c95
PrincePepper <semyon3336@gmail.com>
2020-10-17 20:03:38 +1000

    #Final version

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+# Сайт от куда скачивалась книга уже стал усторевать и он постепенно переежает на новый адрес
+# из-за этого есть лишние действия в коде
+# Семен Середа, 2020
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
+# ссылка нашего учебника на сайте biblio-online
+BASE_URL = 'https://www.biblio-online.ru/viewer/angliyskiy-yazyk-dlya-estestvennonauchnyh-napravleniy-450653#page/1'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+driver = webdriver.Chrome()  # запуск веб драйвера под гугл хром
 
+driver.get('https://www.biblio-online.ru/login')  # переход на страницу авторизации
+time.sleep(5)  # время для прогрузки страницы
+driver.find_element_by_id('go-to-urait').click()  # закрытие диалогового окна, о том что сайт переехал :)
+email = driver.find_element_by_id('login')  # нахождение html элемента по id - login
+email.send_keys('your@email.ru')  # вставка почты
+time.sleep(0.5)
+passwd = driver.find_element_by_name('password')  # нахождение html элемента по id - password
+passwd.send_keys('yourpassword')  # вставка пароля
+time.sleep(0.5)
+next = driver.find_element_by_id('button1id').click()  # авторизация
+time.sleep(5)  # время для прогрузки страницы
+driver.get(BASE_URL)  # переход на страницу нашего учебника
+SCROLL_PAUSE_TIME = 1  # задержка между сколлами страницы
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+last_height = driver.execute_script("return document.body.scrollHeight")
+scheight = 1145  # шаг сколла(у вас может отличаться)
+count = 0  # номер нашей страницы
+while True:
+    time.sleep(SCROLL_PAUSE_TIME)  # ждем прогрузки/прокрутки страницы
+    driver.execute_script("window.scrollTo(0, window.scrollY + %s);" % scheight)  # наш сколл
+    new_height = driver.execute_script("return window.scrollY")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    url_svg = "https://www.biblio-online.ru/viewer/getPage/21BDEACF-1230-4460-A3C6-E2FBDAEB91AE/" + str(count)
+    driver.get(url_svg)
+
+    count += 1
+    if new_height == last_height:
+        break
+    last_height = new_height
